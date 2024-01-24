6e893c34f8e98556c5b5257233b0fffac1e426db
xlwillia <67807020+xlwillia@users.noreply.github.com>
2024-01-24 14:01:35 -0600

    add calculator functionality

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -1,48 +1,52 @@
 <template>
     <div class="calculator">
         <h1 class="text-lg font-semibold p-1">Calculator</h1>
-        <div class="calcScreen p-1">
-            <textarea class="textarea textarea-bordered w-full resize-none" v-model="screen"></textarea>
-        </div>
-        <div class="p-1 grid grid-cols-4">
-            <button type="button" class="btn">C</button>
-            <button type="button" class="btn">SIN</button>
-            <button type="button" class="btn">COS</button>
-            <button type="button" class="btn">TAN</button>
-            <button type="button" class="btn">^</button>
-            <button type="button" class="btn">&Sqrt;</button>
-            <button type="button" class="btn">&divide;</button>
-            <button type="button" class="btn">&times;</button>
-            <button type="button" class="btn">-</button>
-            <button type="button" class="btn">+</button>
-            <button type="button" class="btn">9</button>
-            <button type="button" class="btn">8</button>
-            <button type="button" class="btn">7</button>
-            <button type="button" class="btn">6</button>
-            <button type="button" class="btn">5</button>
-            <button type="button" class="btn">4</button>
-            <button type="button" class="btn">3</button>
-            <button type="button" class="btn">2</button>
-            <button type="button" class="btn">1</button>
-            <button type="button" class="btn">0</button>
-            <button type="button" class="btn">.</button>
-            <button type="button" class="btn">()</button>
-            <button type="button" class="btn col-span-2">=</button>
+        <div class="grid grid-cols-4">
+            <textarea class="textarea textarea-bordered w-full resize-none col-span-4" v-model="screen"></textarea>
+            <button type="button" class="btn" @click="screenClear()">C</button>
+            <button type="button" class="btn" @click="keypadType('SIN')">SIN</button>
+            <button type="button" class="btn" @click="keypadType('COS')">COS</button>
+            <button type="button" class="btn" @click="keypadType('TAN')">TAN</button>
+            <button type="button" class="btn" @click="keypadType('^')">^</button>
+            <button type="button" class="btn" @click="keypadType('sqrt')">&Sqrt;</button>
+            <button type="button" class="btn" @click="keypadType('/')">&divide;</button>
+            <button type="button" class="btn" @click="keypadType('*')">&times;</button>
+            <button type="button" class="btn" @click="keypadType('-')">-</button>
+            <button type="button" class="btn" @click="keypadType('+')">+</button>
+            <button type="button" class="btn" @click="keypadType('9')">9</button>
+            <button type="button" class="btn" @click="keypadType('8')">8</button>
+            <button type="button" class="btn" @click="keypadType('7')">7</button>
+            <button type="button" class="btn" @click="keypadType('6')">6</button>
+            <button type="button" class="btn" @click="keypadType('5')">5</button>
+            <button type="button" class="btn" @click="keypadType('4')">4</button>
+            <button type="button" class="btn" @click="keypadType('3')">3</button>
+            <button type="button" class="btn" @click="keypadType('2')">2</button>
+            <button type="button" class="btn" @click="keypadType('1')">1</button>
+            <button type="button" class="btn" @click="keypadType('0')">0</button>
+            <button type="button" class="btn" @click="keypadType('.')">.</button>
+            <button type="button" class="btn" @click="keypadType('pi')">&pi;</button>
+            <button type="button" class="btn col-span-2" @click="funcEval()">=</button>
         </div>
     </div>
 </template>
 <script>
+    import { evaluate } from "mathjs";
     export default {
-        mounted(){
-
-        },
         data(){
             return {
                 screen: '',
             }
         },
         methods: {
-
+            screenClear(){
+                this.screen = ''
+            },
+            keypadType(key){
+                this.screen = this.screen.concat(key)
+            },
+            funcEval(){
+                this.screen = evaluate(this.screen).toString();
+            }
         }
     }
 </script>
