bf4ff6ba42e29f857c30a947afd756fc7bdaef2e
xlwillia <67807020+xlwillia@users.noreply.github.com>
2024-02-02 12:21:06 -0600

    update bottom navigation active button display

--- src/App.vue
+++ src/App.vue
@@ -19,20 +19,49 @@
   </main>
 
   <div class="btm-nav lg:hidden">
-    <button class="text-primary" @click="$router.push('/')">
+    <button id="calculatorNavBtn" class="text-primary" @click="[$router.push('/'), activeNavButton('calculator')]">
       <span class="material-symbols-outlined">calculate</span>
       <p>Calculator</p>
     </button>
-    <button class="text-primary" @click="$router.push('unitconverter')">
+    <button id="unitNavBtn" class="text-primary" @click="[$router.push('unitconverter'), activeNavButton('unitconverter')]">
       <span class="material-symbols-outlined">measuring_tape</span>
       <p>Unit Converter</p>
     </button>
-    <button class="text-primary" @click="$router.push('currencyconverter')">
+    <button id="currencyNavBtn" class="text-primary" @click="[$router.push('currencyconverter'), activeNavButton('currencyconverter')]">
       <span class="material-symbols-outlined">currency_exchange</span>
       <p>Currency Converter</p>
     </button>
   </div>
 </template>
-<script setup>
-  import { RouterLink, RouterView } from 'vue-router'
+<script>
+  export default {
+    methods: {
+      activeNavButton(button){
+        switch (button) {
+          case 'calculator':
+            document.getElementById("calculatorNavBtn").classList.add('active');
+
+            document.getElementById("unitNavBtn").classList.remove('active');
+            document.getElementById("currencyNavBtn").classList.remove('active');
+            break;
+          case 'unitconverter':
+            document.getElementById("calculatorNavBtn").classList.remove('active');
+
+            document.getElementById("unitNavBtn").classList.add('active');
+
+            document.getElementById("currencyNavBtn").classList.remove('active');
+
+            break;
+          case 'currencyconverter':
+            document.getElementById("calculatorNavBtn").classList.remove('active');
+            document.getElementById("unitNavBtn").classList.remove('active');
+
+            document.getElementById("currencyNavBtn").classList.add('active');
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
 </script>
